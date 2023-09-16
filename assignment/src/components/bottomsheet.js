a5fed0599e6668128dd6546e78c5e0bfdc6a5c97
raja00912cd <mdramiz025@gmail.com>
2023-09-16 12:15:15 +0530

    ready to deploy

--- assignment/src/components/bottomsheet.js
+++ assignment/src/components/bottomsheet.js
@@ -7,6 +7,8 @@ function BottomSheet({ closeModal }) {
             <div className='modal-container'>
                 <h2>Bottom Sheet</h2>
 
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo doloribus harum, tempore nostrum nobis illum officiis facere, modi saepe porro debitis excepturi similique fugit quisquam ipsum assumenda non molestias est! Eum neque ipsam impedit omnis ipsa et voluptates tempore aliquid eos, maxime placeat amet quis nesciunt explicabo aperiam vel deserunt! Quis culpa voluptatum quas quod iusto hic, quasi odit natus?</p>
+
                 <button className='model-btn' onClick={closeModal}>Close</button>
             </div>
         </>
